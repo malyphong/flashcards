81d67303ce73d124ea4c996a2762e87fc08ee338
AJ Mcgladdery and maly phongsavanh <aj@>
2018-08-07 15:11:12 -0700

    toggle all definitions and terms

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,33 @@
 $(document).ready(function(){
-  $("button#functionButton").click(function() {
-      $(".card#functionCard").toggle();
-  });
+//   $("button#functionButton").click(function() {
+//       $(".card#functionCard").toggle();
+//   });
+//
+//   $("#functionCard").click(function() {
+//       $("#term-function").fadeToggle();
+//         $("#def-function").fadeToggle();
+// });
+//
+//   $("button#booleanButton").click(function() {
+//     $("#booleanCard").toggle();
+//   });
+//
+//     $("#booleanCard").click(function() {
+//       $("#term-boolean").fadeToggle();
+//         $("#def-boolean").fadeToggle();
+// });
+//
+// $("button#chainingMethButton").click(function() {
+//   $("#chainingMethCard").toggle();
+// });
 
-  $("#functionCard").click(function() {
-      $("#term-function").fadeToggle();
-        $("#def-function").fadeToggle();
-});
-
-  $("button#booleanButton").click(function() {
-    $("#booleanCard").toggle();
-  });
-
-    $("#booleanCard").click(function() {
-      $("#term-boolean").fadeToggle();
-        $("#def-boolean").fadeToggle();
-});
-
-$("button#chainingMethButton").click(function() {
-  $("#chainingMethCard").toggle();
-});
-
-  $("#chainingMethCard").click(function() {
-    $("#term-chainingMeth").fadeToggle();
-      $("#def-chainingMeth").fadeToggle();
-});
-
+//   $("#chainingMethCard").click(function() {
+//     $("#term-chainingMeth").fadeToggle();
+//       $("#def-chainingMeth").fadeToggle();
+// });
+    $(".card").click(function(){
+      $(this).children("h3").slideToggle();
+      $(this).children(".definitions").slideToggle();
+   });
 
 });
